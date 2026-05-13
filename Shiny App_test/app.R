b6ec50a9695c2a2e5d6605caa78de33634b6e5b9
Artem Povarov <a1povarov@edu.aau.at>
2026-05-13 14:25:07 +0200

    Server, License
    Changed the list for the license, and deployed the app to the server(not gihub pages)

--- Shiny App_test/app.R
+++ Shiny App_test/app.R
@@ -1,7 +1,20 @@
+#install.packages('rsconnect')
+
+library(rsconnect)
 library(shiny)
 library(quarto)
 library(bslib)
 library(lubridate)
+library(tinytex)
+
+# for deployment, just push changes. I will deploy it later.
+#But you can also do it, just remove the comment after all of the code changes
+# and run the command
+# I will do auto deployment later
+# rsconnect::deployApp() 
+#in the console
+
+ 
 
 ui <- page_fluid(
   
@@ -29,7 +42,25 @@ ui <- page_fluid(
      selectInput( 
        inputId = "license", 
        "Select options below:", 
-       list("TEST1" = "TEST1", "TEST2" = "TEST2", "TEST3" = "TEST3") 
+       list(
+         "No License" = "No License",
+         "Academic Free License 3.0" = "Academic Free License 3.0",
+         "Apache License 2.0" = "Apache License 2.0",
+         "Artistic License 2.0" = "Artistic License 2.0",
+         "BSD 2-Clause \"Simplified\" License" = "BSD 2-Clause \"Simplified\" License",
+         "BSD 3-Clause \"New/Revised\" License" = "BSD 3-Clause \"New/Revised\" License",
+         "CC-By Attribution 4.0 International" = "CC-By Attribution 4.0 International",
+         "CC0 1.0 Universal" = "CC0 1.0 Universal",
+         "CC-By Attribution-NonCommercial-NoDerivatives 4.0 International" = "CC-By Attribution-NonCommercial-NoDerivatives 4.0 International",
+         "CC-By Attribution-ShareAlike 4.0 International" = "CC-By Attribution-ShareAlike 4.0 International",
+         "Eclipse Public License 1.0" = "Eclipse Public License 1.0",
+         "GNU General Public License (GPL) 2.0" = "GNU General Public License (GPL) 2.0",
+         "GNU General Public License (GPL) 3.0" = "GNU General Public License (GPL) 3.0",
+         "GNU Lesser General Public License (LGPL) 2.1" = "GNU Lesser General Public License (LGPL) 2.1",
+         "GNU Lesser General Public License (LGPL) 3.0" = "GNU Lesser General Public License (LGPL) 3.0",
+         "MIT License" = "MIT License",
+         "Mozilla Public License 2.0" = "Mozilla Public License 2.0"
+       ) 
      ), 
     
     #textAreaInput("authors", "Authors", rows = 6),
