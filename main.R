7416b39d73033845364cb3adaad731be3c9fe8e8
vhase <valerie.hase@web.de>
2026-09-24 08:59:12 +0200

    create structure for analysis pipeline (not yet filled)

---
+++ main.R
@@ -0,0 +1,7 @@
+#
+# Main Script Preregistering Data Donation Studies
+# Date: 2026-09-24
+
+# We provide the full code but, due to copyright reasons, can only provide some data
+
+# 01 Importing survey data ----------------------------------------------------------------
